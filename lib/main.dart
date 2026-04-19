36df72ca1ba457a605f9ba7d303bb536efac0004
MIlhamRidhoP <ilhamridho273@gmail.com>
2026-04-19 15:33:47 +0700

    feat: menambahkan tampilan splash screen baru

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mabar_slurd/src/feat/common/presentation/views/home_screen.dart';
+import 'package:mabar_slurd/src/feat/common/presentation/views/splash_screen.dart'; // [NEW] Import SplashScreen
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: SplashScreen(), // [UPDATE] Arahkan ke SplashScreen pertama kali
     );
   }
 }
