9c525c1e9fb2aea39d9520b1f6da28220ac7d9bd
faizal <faizalhanif.372@gmail.com>
2026-05-18 10:28:38 +0700

    make all components reusable

--- lib/src/feat/history/presentation/views/page/booking_history_page.dart
+++ lib/src/feat/history/presentation/views/page/booking_history_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mabar_slurd/res/custom_colors.dart';
-import 'package:mabar_slurd/shared/components/mabar_list_card.dart';
+import 'package:mabar_slurd/src/shared/components/mabar_list_card.dart';
 import 'package:mabar_slurd/src/feat/history/presentation/views/widgets/history_data.dart';
 
 class BookingHistoryPage extends StatefulWidget {
